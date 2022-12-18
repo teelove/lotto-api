bc95026773af0b0264cbe35d5055c2ab9a7e7b0d
teelove <androidtohome@gmail.com>
2022-12-18 22:28:48 +0700

    iii

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/insertlocationarrest", function(req, res) {
     console.log('Connected to the database.');
 
     const sql = 'INSERT INTO LocationArrestTB (LocationArrest) VALUES (?)';
-    const values = [req];
+    const values = ['test 2'];
     
 
     connection.execute(sql, values, function (err, result) {
